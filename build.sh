586af4e41d89903cc67c5dc2534735871ea642a4
Tyler Krys <Tyler.Krys@gov.bc.ca>
2022-04-02 18:09:53 -0700

    Add install steps to build script

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ if [ ! -d "./backend/frontend-build" ]; then
   mkdir ./backend/frontend-build
 fi
 
+# Install the front-end dependencies
 cd frontend
+npm install
 
 # Run the build script from Create React App
 npm run build
@@ -14,3 +16,7 @@ cp -R ./build/* ../backend/frontend-build/
 
 # Clean up the frontend/build directory
 rm -R ./build
+
+# Install the back-end dependencies
+cd ../backend
+npm install
