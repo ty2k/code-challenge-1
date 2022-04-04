9ecbbb1461f3fbd4334a0cb87d215cb3078a108c
Tyler Krys <Tyler.Krys@gov.bc.ca>
2022-04-03 22:07:47 -0700

    Refactor front-end API server calls to remove localhost reference

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -32,7 +32,7 @@ const PlaceItem = props => {
     setShowConfirmModal(false);
     try {
       await sendRequest(
-        `http://localhost:5000/api/places/${props.id}`,
+        `/api/places/${props.id}`,
         'DELETE',
         null,
         {
@@ -84,7 +84,7 @@ const PlaceItem = props => {
           {isLoading && <LoadingSpinner asOverlay />}
           <div className="place-item__image">
             <img
-              src={`http://localhost:5000/${props.image}`}
+              src={`/${props.image}`}
               alt={props.title}
             />
           </div>
