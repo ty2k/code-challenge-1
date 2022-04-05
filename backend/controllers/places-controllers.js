03fbdf80e672d5202ae4fe7e7dd60fe8207e0e83
Tyler Krys <Tyler.Krys@gov.bc.ca>
2022-04-04 23:04:21 -0700

    Refactor getPlacesByUserId function to return a Place query with the User schema attached

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -78,29 +78,23 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  // let places;
-  let userWithPlaces;
+  let placesWithUser;
   try {
-    userWithPlaces = await User.findById(userId).populate('places');
+    placesWithUser = await Place.find({ creator: userId }).populate('creator');
   } catch (err) {
     const error = new HttpError(
-      'Fetching places failed, please try again later.',
+      'Something went wrong, could not find any places.',
       500
     );
     return next(error);
   }
 
-  // if (!places || places.length === 0) {
-  if (!userWithPlaces || userWithPlaces.places.length === 0) {
-    return next(
-      new HttpError('Could not find places for the provided user id.', 404)
-    );
+  if (placesWithUser.length === 0) {
+    return next(new HttpError('Could not find places.', 404));
   }
 
   res.json({
-    places: userWithPlaces.places.map(place =>
-      place.toObject({ getters: true })
-    )
+    places: placesWithUser.map((place) => place.toObject({ getters: true })),
   });
 };
 
